a9276f0bddee97589eeb2264c625fc415a7b8cd6
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-27 21:47:49 +0100

    Add Mod_02, lesson 04 - Types of Data)

---
+++ 6. JS BASIC/Mod_02/lezioni/lezione_04.js
@@ -0,0 +1,50 @@
+/* Lezione 04 - Tipi di Dati */
+
+// Numeri interi
+let number = 19;
+console.log(number);
+
+// Numeri decimale
+let floatNumber = 20.3;
+console.log(floatNumber);
+
+// Numeri positivi
+let posNumber = +20;
+console.log(posNumber);
+
+// Numeri negativi
+let negNumber = -20;
+console.log(negNumber);
+
+// String
+let nome = "Nicolau";
+
+let message = "Hello, ";
+let person = "Alfredo";
+
+let greeting = message + person;
+console.log(greeting);
+
+// Boolean
+let yes = true;
+console.log(yes);
+
+let no = false
+console.log(no);
+
+// Boolean Operators
+let isRaining = true
+let iOwnAnUmbrella = false
+
+// Operatore and (&&)
+// let shouldITakeAnUmbrella = isRaining && iOwnAnUmbrella
+// console.log(shouldITakeAnUmbrella)
+
+// Operatore or (||)
+let shouldITakeAnUmbrella = isRaining || iOwnAnUmbrella
+console.log(shouldITakeAnUmbrella)
+
+// Tipo null and undefined
+let n
+
+n = 5
