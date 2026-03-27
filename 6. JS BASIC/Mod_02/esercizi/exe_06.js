7a57e8d2acb4fae556aa173d77d1bafa93ec9876
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-27 21:55:33 +0100

    Add Mod_02, exercise - Type of Data - Casting (Boolean)

---
+++ 6. JS BASIC/Mod_02/esercizi/exe_06.js
@@ -0,0 +1,13 @@
+let hello = "Ciao"; //boolean
+let age = 18; //string
+let isGraduated = false; //number
+
+// casting
+let helloBoolean = Boolean(hello);
+let ageString = String(age);
+let isGraduatedNumber = Number(isGraduated);
+
+// stampa risultati
+console.log(helloBoolean);
+console.log(ageString);
+console.log(isGraduatedNumber);
