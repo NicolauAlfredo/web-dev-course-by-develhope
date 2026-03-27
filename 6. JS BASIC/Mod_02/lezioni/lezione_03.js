e0b6d132becce2a863e53109b504ac04f913c85b
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-27 21:46:13 +0100

    Add Mod_02, lesson 03 - Types of variables (let, const and var)

---
+++ 6. JS BASIC/Mod_02/lezioni/lezione_03.js
@@ -0,0 +1,16 @@
+/* Lezione 03 - let, const e var */
+
+// Usata quando il valore può cambiare
+let nome = "Nicolau";
+console.log(nome)
+
+// Usata quando il valore NON cambia
+const idNumber = 10;
+console.log(idNumber)
+
+// Può essere ridichiarata e modificata
+var eta = 20;
+console.log(eta)
+
+eta = 28
+console.log(eta)
