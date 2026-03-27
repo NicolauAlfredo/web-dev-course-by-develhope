a56984579c60006b2c061ee6dcbadf154f52c0bc
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-27 21:52:00 +0100

    Add Mod_02, exercise - Variables)

---
+++ 6. JS BASIC/Mod_02/esercizi/exe_01.js
@@ -0,0 +1,24 @@
+/* Crea due variabili che rappresentano la base e l'altezza di un rettangolo e assegna un valore ad esse */
+const rectangleWidth = 10;
+const rectangleHeight = 5;
+
+/* Crea una variabile che rappresenta il raggio di un cerchio */
+const circleRadius = 7;
+
+/* Crea tre variabili che rappresentano il prezzo di tre cibi a tua scelta, 
+per esempio pizza, focaccia e mela, e assegna a ciascuna di loro un valore. */
+const meatPrice = 10;
+const breadPrice = 1;
+const porkPrice = 8;
+
+/* Stampa sulla console il valore delle variabili appena create. */
+console.log(rectangleWidth);
+console.log(rectangleHeight);
+console.log(circleRadius);
+console.log(meatPrice);
+console.log(breadPrice);
+console.log(porkPrice);
+
+/* Crea una variabile che contiene al suo interno la somma di due numeri, moltiplicata per due. */
+let sumResult = (3 + 4) * 2;
+console.log(sumResult);
