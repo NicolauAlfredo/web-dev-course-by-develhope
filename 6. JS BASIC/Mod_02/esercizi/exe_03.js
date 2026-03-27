795dc89c4041abd8fb2c5434e11ad97c71cfb0c5
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-27 21:53:24 +0100

    Add Mod_02, exercise - Names of Variables)

---
+++ 6. JS BASIC/Mod_02/esercizi/exe_03.js
@@ -0,0 +1,13 @@
+const rectangleWidth = 2;
+const rectangleHeight = 6;
+
+const rectangleArea = rectangleWidth * rectangleHeight;
+
+console.log(
+  "L'area di un rettangolo di base: " +
+    rectangleWidth +
+    " e altezza: " +
+    rectangleHeight +
+    " è uguale a: " +
+    rectangleArea,
+);
