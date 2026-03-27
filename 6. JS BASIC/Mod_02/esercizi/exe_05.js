25486cc95893c9434111cd5f479df8899551ca90
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-27 21:54:59 +0100

    Add Mod_02, exercise - Type of Data (Boolean)

---
+++ 6. JS BASIC/Mod_02/esercizi/exe_05.js
@@ -0,0 +1,9 @@
+let itIsRaining = false;
+let isSunny = true;
+let isWeekend = true;
+let hasHomework = false;
+let isHungry = true;
+
+console.log(
+  `itIsRaining: ${itIsRaining}, isSunny: ${isSunny}, isWeekend: ${isWeekend}, hasHomework: ${hasHomework}, isHungry: ${isHungry}`,
+);
