8ab571324f4fd4e75ca6082c27aa411a17a74fbc
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-28 17:04:39 +0100

    Add Mod_03, esercise 15 - Conditional (if/else) Operator

---
+++ 6. JS BASIC/Mod_03/esercizi/exe_15.js
@@ -0,0 +1,10 @@
+let name;
+let surname = "Rossi";
+const fullname = name + " " + surname;
+
+// Controllo con if-else
+if (name && surname) {
+  console.log(fullname);
+} else {
+  console.log("Fullname is invalid");
+}
