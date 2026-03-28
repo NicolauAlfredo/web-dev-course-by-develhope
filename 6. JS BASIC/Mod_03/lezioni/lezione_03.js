d19bb5ca828a7318976b52a9e5e389b109afc3cc
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-28 17:11:20 +0100

    Add Mod_03, lesson - Conditional (ternary) Operator

---
+++ 6. JS BASIC/Mod_03/lezioni/lezione_03.js
@@ -0,0 +1,12 @@
+/* Lezione 03 - L'operatore Ternario */
+
+let isRaining = true;
+let isCloudy = false;
+
+let result = isRaining
+  ? "I should take an umbrella!"
+  : isCloudy
+    ? "I could take an umbrella!"
+    : "It's a sunny day!";
+
+console.log(result);
