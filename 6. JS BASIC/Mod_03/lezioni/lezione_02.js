3ac8d22dd0d89b106821fa307530158dd9cb3eb6
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-28 16:49:11 +0100

    Add Mod_03, lesson 02 - Conditional (if/else) Operator

---
+++ 6. JS BASIC/Mod_03/lezioni/lezione_02.js
@@ -0,0 +1,14 @@
+/* Lezione 02 - Il costrutto if/else */
+
+let isRaining = false;
+let isCloudy = true;
+
+if (isRaining) {
+  console.log("I should take an umbrella!");
+} else if (isCloudy) {
+  console.log("I could take an umbrella!");
+} else {
+  console.log("It's a sunny day");
+}
+
+console.log("Done");
