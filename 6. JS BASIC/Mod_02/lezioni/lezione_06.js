9523dc8a31c1c2d6286269f37ccdbe26874416b1
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-28 15:53:22 +0100

    Add Mod_02, lesson 06 - Comparison Operators

---
+++ 6. JS BASIC/Mod_02/lezioni/lezione_06.js
@@ -0,0 +1,32 @@
+/* Lezione 06 - Gli operatori di confronto */
+
+// Equality Operator on numbers (==)
+let n1 = 5;
+let n2 = 4;
+
+console.log(n1 == n2);
+
+// Loss Equality Operator on Strings (==)
+n1 = "5";
+console.log(n1 == n2);
+
+// Strict Equality  (===)
+console.log(n1 === n2);
+
+// Greater than 
+console.log(n1 > n2)
+
+// Less than 
+console.log(n1 < n2)
+
+// Not equal
+console.log(n1 != n2)
+
+// Strict not equal 
+console.log(n1 !== n2)
+
+// Greater than or equal to
+console.log(n1 >= n2)
+
+// Less than or equal to
+console.log(n1 <= n2)
