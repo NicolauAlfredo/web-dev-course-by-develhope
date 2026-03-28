17d9ec35c22fdfd0c4cc32aff46069a46258f3a5
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-28 17:43:03 +0100

    Add Mod_03, lesson 04 - Conditional (switch) Operator

---
+++ 6. JS BASIC/Mod_03/lezioni/lezione_04.js
@@ -0,0 +1,17 @@
+/* Lezione 04 - Il costrutto switch */
+
+let food = "pizza";
+
+switch (food) {
+  case "pizza":
+    console.log("I like pizza!");
+    break;
+
+  case "burrito":
+    console.log("Burritos are great!");
+    break;
+
+  default:
+    console.log("I don't know this food!");
+    break;
+}
