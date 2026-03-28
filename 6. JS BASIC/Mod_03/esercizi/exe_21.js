4bb16184579c46fa68a0d18790144545e011b7a8
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-28 17:45:10 +0100

    Add Mod_03, esercise 21 - Conditional (switch) Operator

---
+++ 6. JS BASIC/Mod_03/esercizi/exe_21.js
@@ -0,0 +1,15 @@
+let tariffa = "BB";  
+
+switch (tariffa) {
+  case "BB":
+    console.log("Prezzo della camera: 50€");
+    break;
+  case "HB":
+    console.log("Prezzo della camera: 80€");
+    break;
+  case "FB":
+    console.log("Prezzo della camera: 100€");
+    break;
+  default:
+    console.log("Tariffa non valida");
+}
