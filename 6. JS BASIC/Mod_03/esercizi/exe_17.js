4d66c87a19839342e70fe3d3c145db8155463965
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-28 17:05:02 +0100

    Add Mod_03, esercise 17 - Conditional (if/else) Operator

---
+++ 6. JS BASIC/Mod_03/esercizi/exe_17.js
@@ -0,0 +1,12 @@
+const valore = 12;
+
+// Verifica il tipo della variabile
+if (typeof valore === "number") {
+  console.log("The type of valore is a number");
+} else if (typeof valore === "string") {
+  console.log("The type of valore is a string");
+} else if (typeof valore === "boolean") {
+  console.log("The type of valore is a boolean");
+} else {
+  console.log("The type of valore is unknown");
+}
