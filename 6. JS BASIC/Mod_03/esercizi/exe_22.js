a86ad301458d435e133d767e872888e5f475d43a
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-28 17:48:54 +0100

    Add Mod_03, esercise 22 - Conditional (switch) Operator

---
+++ 6. JS BASIC/Mod_03/esercizi/exe_22.js
@@ -0,0 +1,12 @@
+let favoriteFood = "coconut";
+
+switch (favoriteFood.toLowerCase()) {
+  case "pizza":
+    console.log("You like PIZZA!");
+    break;
+  case "coconut":
+    console.log("You like COCONUT!");
+    break;
+  default:
+    console.log("You like something else!");
+}
