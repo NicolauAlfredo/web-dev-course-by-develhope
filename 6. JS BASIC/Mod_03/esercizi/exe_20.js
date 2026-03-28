db7bf2aa549f773cc50eb3cc0756b2bd0456a9fb
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-28 17:34:17 +0100

    Add Mod_03, esercise 20 - Conditional (ternary) Operator

---
+++ 6. JS BASIC/Mod_03/esercizi/exe_20.js
@@ -0,0 +1,10 @@
+const isSunnyDay = true;
+const isHomeworkCompleted = true;
+
+// Utilizzo dell'operatore ternario per verificare entrambe le condizioni
+const result =
+  isSunnyDay && isHomeworkCompleted
+    ? "Jesse can go out to play"
+    : "Jesse stays at home";
+
+console.log(result);
