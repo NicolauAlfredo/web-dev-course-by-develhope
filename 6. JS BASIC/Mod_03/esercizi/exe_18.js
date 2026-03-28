cb6f337bb636edfbdb3969b6c802f1083c840d3c
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-28 17:33:05 +0100

    Add Mod_03, esercise 18 - Conditional (switch) Operator

---
+++ 6. JS BASIC/Mod_03/esercizi/exe_18.js
@@ -0,0 +1,4 @@
+const age = 10; // Assegna un valore numerico a age
+
+// Utilizzo dell'operatore ternario per verificare se l'età è maggiore o uguale a 18
+console.log(age >= 18 ? "You can drive a car" : "You are too young to drive");
