b6d0f3d7681dd0704ddf3de942068336a1cdd334
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-29 15:50:30 +0200

    Add Mod_03, lesson 05 - Loops (for and while)

---
+++ 6. JS BASIC/Mod_03/lezioni/lezione_05.js
@@ -0,0 +1,15 @@
+/* Lezione 05 - I loop */
+
+// for
+let total = 0;
+for (let i = 0; i <= 10; i = i + 1) {
+  total = total + 1;
+  console.log(total);
+}
+
+// while
+let i = 0;
+while (i < 10) {
+  console.log(i);
+  i++;
+}
