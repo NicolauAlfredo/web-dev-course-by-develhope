fff952f49367073ad3cba68a5e7ab0012baed001
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-03-29 19:11:00 +0200

    Add Mod_04, lesson 01 - Le funzioni

---
+++ 6. JS BASIC/Mod_04/lezioni/lezione_01.js
@@ -0,0 +1,14 @@
+/* Lezione 01 - Le funzioni */
+
+function somma(a, b) {
+  return a + b;
+}
+
+let result = somma(12, 4);
+console.log(result);
+
+function sayHello(name) {
+  console.log("Hello, " + name + "!");
+}
+
+sayHello("Nicolau");
