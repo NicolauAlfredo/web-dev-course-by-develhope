96cb3597fd0fc6fabb7600676d3914eca5aa048c
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-04-09 15:59:52 +0200

    Add Mod_05, lesson 05 - Il metodo costruttore

--- 6. JS BASIC/Mod_05/lezioni/lezione_05.js
+++ 6. JS BASIC/Mod_05/lezioni/lezione_05.js
@@ -1,4 +1,4 @@
-/* Lezione 02 - Il metodo costruttore */
+/* Lezione 05 - Il metodo costruttore */
 
 // Creare una funzione costruttrice
 
@@ -20,6 +20,6 @@ function Person(name, surname, age, profession) {
 let person1 = new Person("Nicolau", "Alfredo", 22, "Programmer");
 let person2 = new Person("Alfredo", "Leão", 40, "UX/UI Design");
 
-person1.sayHello();
+person1.sayHello(); 
 
 person2.sayHello();
