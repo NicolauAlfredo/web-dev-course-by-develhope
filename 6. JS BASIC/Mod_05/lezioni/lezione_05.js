24aed546ac665f2eabad764f48e137fb0f91b40e
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-04-09 19:41:31 +0200

    Add Mod_05, lesson 05 - Il metodo costruttore

--- 6. JS BASIC/Mod_05/lezioni/lezione_05.js
+++ 6. JS BASIC/Mod_05/lezioni/lezione_05.js
@@ -3,23 +3,23 @@
 // Creare una funzione costruttrice
 
 function Person(name, surname, age, profession) {
-  ((this.name = name),
-    (this.surname = surname),
-    (this.age = age),
-    (this.profession = profession),
-    (this.growUp = function () {
-      this.age += 1;
-    }),
-    (this.sayHello = () => {
-      console.log(
-        `Hello, my name is ${this.name} ${this.surname} and I am ${this.age} years old`,
-      );
-    }));
+  this.name = name;
+  this.surname = surname;
+  this.age = age;
+  this.profession = profession;
+  this.growUp = function () {
+    this.age += 1;
+  };
+  this.sayHello = () => {
+    console.log(
+      `Hello, my name is ${this.name} ${this.surname} and I am ${this.age} years old`,
+    );
+  };
 }
 
 let person1 = new Person("Nicolau", "Alfredo", 22, "Programmer");
 let person2 = new Person("Alfredo", "Leão", 40, "UX/UI Design");
 
-person1.sayHello(); 
+person1.sayHello();
 
 person2.sayHello();
