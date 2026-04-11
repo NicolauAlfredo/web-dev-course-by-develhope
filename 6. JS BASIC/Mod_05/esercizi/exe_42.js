d52a803ce422e0556ec4658c07856b8c113031e8
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-04-11 17:46:09 +0200

    Add Mod_05, exercise 42 - Il metodo costruttore

---
+++ 6. JS BASIC/Mod_05/esercizi/exe_42.js
@@ -0,0 +1,14 @@
+const student = {
+  id: 1,
+  school: "Liceo",
+  year: 3,
+  personalData: {
+    // oggetto annidato
+    name: "Nicolau",
+    surname: "Alfredo",
+    age: 16,
+  },
+};
+
+// Stampa l'oggetto annidato personalData
+console.log(student.personalData);
