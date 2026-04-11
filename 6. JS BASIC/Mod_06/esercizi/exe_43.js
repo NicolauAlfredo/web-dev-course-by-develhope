eabcfbeae4d6ab273e68c38d23c73a66abe4512b
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-04-11 16:22:44 +0200

    Add Mod_06, exercise 43 - Introduzione agli array

---
+++ 6. JS BASIC/Mod_06/esercizi/exe_43.js
@@ -0,0 +1,26 @@
+// Creiamo un array chiamato students
+let students = [
+  {
+    id: 1,
+    name: "Joao",
+    surname: "de Almeida",
+    age: 20,
+  },
+  {
+    id: 2,
+    name: "Amilca",
+    surname: "Cabral",
+    age: 22,
+  },
+  {
+    id: 3,
+    name: "Pedro",
+    surname: "Francisco",
+    age: 19,
+  },
+];
+
+// Stampare le info del primo studente
+console.log(students[0]);
+// Gli array partono da indice 0
+// quindi students[0] è il primo oggetto (Joao de Almeida) 
