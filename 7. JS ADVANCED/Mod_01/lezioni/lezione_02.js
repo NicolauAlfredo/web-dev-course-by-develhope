57a24fb6c0e34b6d4372635c6a15bbe4517d7c4f
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-04-21 02:03:40 +0200

    Add Mod_01, lesson 02 - I template strings

---
+++ 7. JS ADVANCED/Mod_01/lezioni/lezione_02.js
@@ -0,0 +1,12 @@
+/* Lezione 02 - I template strings */
+
+const firstName = "Nicolau";
+
+function gateName() {
+  return "Alfredo";
+}
+
+// const myString = "Hello, " + firstName;
+const myString = `Hello, ${firstName}`;
+
+console.log(firstName);
