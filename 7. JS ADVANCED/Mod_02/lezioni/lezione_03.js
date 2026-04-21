3f1d5284eb61838cb6561d96c4e283f7a6cf4cbf
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-04-21 23:06:17 +0200

    Add Mod_02, lesson 03 - Gestire gli errori con le callback

--- 7. JS ADVANCED/Mod_02/lezioni/lezione_03.js
+++ 7. JS ADVANCED/Mod_02/lezioni/lezione_03.js
@@ -1,2 +1,17 @@
 /* Lezione 03 - Gestire gli errori con le callback */
 
+function checkAge(age, callback) {
+  if (age >= 18) {
+    callback(null, "OK");
+  } else {
+    callback(new Error("Ko"), null);
+  }
+}
+
+checkAge(18, (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
