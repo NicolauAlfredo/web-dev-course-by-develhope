77e27b5f180a861cd25452bfe3ae31dcf6239820
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-04-21 21:59:12 +0200

    Add Mod_01, exercise 65 - La destrutturazione

---
+++ 7. JS ADVANCED/Mod_01/esercizi/exe_65.js
@@ -0,0 +1,7 @@
+const colors = ["red", "green", "blue"];
+
+const [firstColor, secondColor, ThirdColor] = colors;
+
+console.log(firstColor);  // "red"
+console.log(secondColor); // "green"
+console.log(ThirdColor);  // "blue"
