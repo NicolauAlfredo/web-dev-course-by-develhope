11315e93e3cddbf13f1fc5215f8b849cf58b7102
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-05-08 14:49:36 +0200

    Add Mod_01, exercise 93 - Cercare elementi nel DOM

---
+++ 8. JS DOM/Mod_01/esercizi/exe_93.js
@@ -0,0 +1,17 @@
+// Cerca tutti gli elementi con la classe "paragrafo"
+const paragrafi = document.querySelectorAll(".paragrafo");
+
+// Stampa tutti i paragrafi nel console
+console.log(paragrafi);
+
+// Cerca l'elemento con id "elemento-3"
+const elementoTre = document.getElementById("elemento-3");
+
+// Stampa l'elemento trovato
+console.log(elementoTre);
+
+// Cerca tutti gli elementi h2 della pagina
+const titoli = document.querySelectorAll("h2");
+
+// Stampa tutti gli h2 nel console
+console.log(titoli);
