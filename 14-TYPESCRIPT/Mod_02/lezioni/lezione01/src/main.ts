a58e1e1bfa7f98812a345e00d4ad0635e5b3f91c
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-05 13:27:13 +0200

    Add Mod_02, lesson 01 - I tipi primitivi

---
+++ 14-TYPESCRIPT/Mod_02/lezioni/lezione01/src/main.ts
@@ -0,0 +1,8 @@
+let nome: string = "Nicolau Alfredo";
+let age: number = 27;
+let profession: string = "Full Stack Java Developer";
+let openToWork: boolean = false;
+
+console.log(
+  `Piacere, mi chiamo ${nome}, ho ${age}, sono un ${profession}, sto cercando lavoro? ${openToWork}`,
+);
