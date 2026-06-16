8750584e7b0a7b766b6af53414bee30b0b5f457b
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-16 21:49:02 +0200

    Add Mod_07, lesson 01 - Enums

---
+++ 14-TYPESCRIPT/Mod_07/lezioni/lezione01/src/main.ts
@@ -0,0 +1,30 @@
+enum Direction {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+function checkDirection(direction: Direction) {
+  switch (direction) {
+    case Direction.Up:
+      console.log("Going UP");
+      break;
+    case Direction.Down:
+      console.log("Going DOWN");
+      break;
+    case Direction.Left:
+      console.log("Going LEFT");
+      break;
+    case Direction.Right:
+      console.log("RIGHT");
+      break;
+    default:
+      console.log("Invalid!!");
+      break;
+  }
+}
+
+const myDirection: Direction = Direction.Right;
+
+console.log(checkDirection(myDirection));
