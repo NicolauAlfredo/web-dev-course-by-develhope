5e1e1e79eeeb516fb6ad505ab4bbe1a812c2a3d8
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-25 01:20:35 +0200

    Add Mod_05, exercise 137 - React Query

---
+++ 16-REACT/Mod_05/esercizi/esercizio137/src/App.tsx
@@ -0,0 +1,11 @@
+import { GithubUser } from "./components/GithubUser";
+
+function App() {
+  return (
+    <main>
+      <GithubUser username="NicolauAlfredo" />
+    </main>
+  );
+}
+
+export default App;
